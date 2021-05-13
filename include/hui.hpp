5933dc8d4ddf53c28bd4e01a3f4f01a646d17559
Hunter Land <hunterland457@gmail.com>
2021-05-13 02:59:41 -0700

    Removed old header

--- include/hui.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "./element.hpp"
-#include "./set.hpp"
-#include "./button.hpp"
-#include "./item.hpp"
-#include "./itemHolder.hpp"
-#include "./scrollBar.hpp"
-#include "./textField.hpp"
